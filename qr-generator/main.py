293853674712ceb391257d53f5c83f3ccd56a5c1
ashuk-io <ashutoshjpis.ncs@gmail.com>
2026-06-16 20:33:17 +0530

    reorganize

--- qr-generator/main.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-url = input("Enter URL: ")
-file = input("Enter filename to save it as : ")
-if not(file.endswith('.png')):
-    file = file+'.png'
-qr = qrcode.make(url)
-qr.save(file)   
